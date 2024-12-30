2fe32122dda5b0fbb69cb1aef15317aca3d0a75e
TodTheMatsu <gokudollmeanak@gmail.com>
2024-12-30 11:04:35 +0700

    first hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-
-import './App.css'
+import { GlowCapture, Glow } from '@codaworks/react-glow'
+import {motion} from 'framer-motion'
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <GlowCapture>
+      <div className='bg-black h-screen w-screen flex items-center flex-col justify-center'>
+        <div></div>
+        <div className=' flex items-center justify-center flex-col'>
+          <h1 className='text-white text-9xl absolute'>Vanish</h1>
+          <h1 className='text-white blur-md text-9xl '>Vanish</h1>
+        </div>
+        <h2 className='text-white text-4xl '>Post. Share. Vanish.</h2>
+        <Glow>
+        <button className='boxdesign text-center my-5 flex flew-grow items-center justify-center px-5 py-1'>Explore</button>
+        </Glow>
+      </div>
+    </GlowCapture>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import reactglow from '@codaworks/react-glow'
+
 export default {
   content: [
     "./index.html",
@@ -7,5 +9,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [ require('@codaworks/react-glow/tailwind')],
 }
