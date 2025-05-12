83e429cb30757df232622807b09fd3a9d9115e4d
Lee <88866038+TodTheMatsu@users.noreply.github.com>
2025-05-12 08:33:50 +0000

    you can search the player from clicking on them on their posts"

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import { Post } from '../hooks/usePosts';
+import { Link } from 'react-router-dom';
 
 interface PostListProps {
   posts: Post[];
@@ -21,18 +22,18 @@ export function PostList({ posts }: PostListProps) {
           animate={{ opacity: 1, y: 0 }}
           className="p-6 bg-neutral-900 border border-neutral-800 rounded-xl hover:border-neutral-700 transition-all break-words"
         >
-          <div className="flex items-center space-x-3 mb-4">
+          <Link to={`/profile/${post.author.username}`} className="inline-flex items-center space-x-3 mb-4">
             <img 
               src={post.author.profilePicture} 
               alt="Profile" 
-              className="w-8 h-8 rounded-full object-cover" 
+              className="w-8 h-8 rounded-full  object-cover" 
               onError={(e) => { e.currentTarget.onerror = null; e.currentTarget.src = "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmo5MXJsb2U4ZDVlNjU5dzJ4NGRpanY0YTJ0Zm16MnBleHJxMWx1ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41m0CPz6UCnaUmxG/giphy.gif" }}
             />
-            <div>
+            <div className="hover:underline hover:scale-105 transition-all duration-300">
               <span className="font-bold">{post.author.displayName}</span>
               <span className="text-neutral-400 text-sm ml-2">@{post.author.username}</span>
             </div>
-          </div>
+          </Link>
           <div className="min-h-[50px] mb-4">
             <p className="text-lg whitespace-pre-wrap">
               {post.content.split(/(\s+)/).map((seg, i) => {
